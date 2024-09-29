ad4ecfb6db3d2756d169511a0938f0de707de480
Octopus142 <blackfairyforma726@gmail.com>
2024-09-29 07:26:32 +0100

    Update SUM.c

--- 112/SUM.c
+++ 112/SUM.c
@@ -9,17 +9,19 @@
 #include "antipyramid"
 #include "tri"
 #include "antitri"
-const DET = "det1234"+"det5678"+"det9101112"
-const tri = "tri - antitri"
-const ONE = "one" + "oppoone"
-const Square = "square * antisquare"
-const Pyramid = "pyramid % antipyramid"
-const DETtri = "DET * tri"
-const triDET = "tri % DET"
-const ONEsquare = "ONE * square"
-const squareONE = "square % ONE"
-const oppoPyramid = "antipyramid / pyramid"
-const dimPyramid = "square - antisquare"
+void constance(){
+const DET = "det1234"+"det5678"+"det9101112";
+const tri = "tri - antitri";
+const ONE = "one" + "oppoone";
+const Square = "square * antisquare";
+const Pyramid = "pyramid % antipyramid";
+const DETtri = "DET * tri";
+const triDET = "tri % DET";
+const ONEsquare = "ONE * square";
+const squareONE = "square % ONE";
+const oppoPyramid = "antipyramid / pyramid";
+const dimPyramid = "square - antisquare";
+}
 void SUM(){
     void ["DET/tri+ tri/DET"===DETtri + triDET]
     void [DETtri+ONEsquare===squareONE+triDET]
