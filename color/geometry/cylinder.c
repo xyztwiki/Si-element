202521807365a920ff58feaa29c7f0d996677533
Octopus142 <blackfairyforma726@gmail.com>
2024-09-29 07:20:34 +0100

    Update cylinder.c

--- color/geometry/cylinder.c
+++ color/geometry/cylinder.c
@@ -1,121 +1,121 @@
 #include "color/color.h"
-#include "color/chemistry"
+#include "color/fourcolor
 void CylinderXY(){
-    const element1 =
+    const element1[4][4] =
         {{0,1,0,1},
         {1,0,1,0},
         {0,0,0,1},
         {0,0,0,0}}
-    const element2 =
+    const element2[4][4] =
         {{0,1,0,1},
         {0,0,1,0},
         {0,1,0,1},
         {0,0,0,0}}
-    const element3 =
+    const element3[4][4] =
         {{0,1,0,1},
         {0,0,1,0},
         {0,0,0,1},
         {0,0,1,0}}
-    const element4 =
+    const element4[4][4] =
         {{0,1,0,1},
         {0,0,1,0},
         {0,0,0,1},
         {1,0,0,0}}
 }
 void CylinderYZ(){
-    const element5 =
+    const element5[4][4] =
            {{1,0,1,0},
             {0,1,0,0},
             {0,0,1,0},
             {0,0,0,1}}
 
-    const element6 =
+    const element6[4][4] =
            {{1,0,0,0},
             {0,1,0,1},
             {0,0,1,0},
             {0,0,0,1}}
 
-    const element7 =
+    const element7[4][4] =
            {{1,0,0,0},
             {0,1,0,0},
             {0,0,1,0},
             {0,1,0,1}}
 
-    const element8 =
+    const element8[4][4] =
            {{1,0,0,0},
             {0,1,0,0},
             {1,0,1,0},
             {0,0,0,1}}
 }
 void CylinderXZ() {
-    const element9 =
+    const element9[4][4] =
            {{0,1,0,1},
             {1,0,1,0},
             {0,0,0,1},
             {0,0,0,0}}
 
-    const element10 =
+    const element10[4][4] =
            {{0,1,0,1},
             {0,0,1,0},
             {0,1,0,1},
             {0,0,0,0}}
 
-    const element11 =
+    const element11[4][4] =
            {{0,1,0,1},
             {0,0,1,0},
             {0,0,0,1},
             {0,0,1,0}}
 
-    const element12 =
+    const element12[4][4] =
            {{0,1,0,1},
             {0,0,1,0},
             {0,0,0,1},
             {1,0,0,0}}
 }
 void CylinderXYZ(){
-    const element13 =
+    const element13[4][4] =
         {{1,0,1,0},
             {0,0,0,1},
             {1,0,0,0},
             {0,1,0,0}}
 
-    const element14 =
+    const element14[4][4] =
         {{0,0,1,0},
             {0,1,0,1},
             {1,0,0,0},
             {0,1,0,0}}
 
-    const element15 =
-        {{0,0,1,0},
+    const element15[4][4] =
+           {{0,0,1,0},
             {0,0,0,1},
             {1,0,1,0},
             {0,1,0,0}}
 
-    const element16 =
+    const element16[4][4] =
            {{0,0,1,0},
             {0,0,0,1},
             {1,0,0,0},
             {0,1,0,1}}
 
-    const element17 =
+    const element17[4][4] =
            {{0,1,0,0},
             {1,0,0,0},
             {0,1,0,0},
             {1,0,1,0}}
 
-    const element18 =
+    const element18[4][4] =
            {{0,0,0,0},
             {1,0,1,0},
             {0,1,0,0},
             {1,0,1,0}}
 
-    const element19 =
+    const element19[4][4] =
            {{0,0,0,0},
             {1,0,0,0},
             {0,1,0,1},
             {0,0,1,0}}
 
-    const element20 =
+    const element20[4][4] =
            {{0,0,0,1},
             {1,0,0,0},
             {0,1,0,0},
