f6f178c283054cf65ddb978077237e6e72e6c624
Octopus142 <blackfairyforma726@gmail.com>
2024-10-07 12:49:34 +0100

    Update RAM.vb

--- C/VB/RAM.vb
+++ C/VB/RAM.vb
@@ -2,7 +2,7 @@ Imports System.Runtime.InteropServices
 Public Class RAMSimulator
     Private ramArray As Byte()
     Public Sub New(sizeInBytes As Integer)
-        ramArray = New Byte(sizeInBytes - 1) {}
+        ramArray = New Byte(sizeInBytes + 1) {}
     End Sub
     Public Function ReadByte(address As Integer) As Byte
         Return ramArray(address)
