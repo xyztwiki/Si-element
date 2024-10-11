37a6f8fde22ac556fed1339a2d53f28902ff82d4
Octopus142 <blackfairyforma726@gmail.com>
2024-10-11 12:27:46 +0800

    Update CH4.c

--- color/chemistry/CH4.c
+++ color/chemistry/CH4.c
@@ -3,10 +3,10 @@ void CH4(int C, double H) {
     double H4 = H*H + H*H;
 }
 void C5H12(int C, double H) {
-   int C5 = C + C* C;
-   double H4 = H*H + H*H + H*H + H*H;
+   int C5 = C + (C * C) ^ 2;
+   double H12 = (H*H + H*H) ^ 3;
 }
 void C17H36(int C, double H) {
-    int C17 = 17;
-    double H36 = 36;
+    int C17 = (C * C) ^ 4 + C
+    double H36 = (H * H + H * H + H * H) ^ 6;
 }
